3ad25a0f554d7d190c5f1e4ef15b9bbacfb86439
Naexa-glitch <nashleycedeno@gmail.com>
2023-05-24 00:31:41 -0600

    Para esta sexta entrega se implementó VUE a adminlogin.html y a changepassword.html, también se crearon dos nuevos componentes: LoginAdmin.js y ChangePassword, llevando sus respectivos formularios para interactuar a futuro con estos.

---
+++ dist/js/components/ChangePassword.js
@@ -0,0 +1,45 @@
+app.component('change-password',{
+
+    data() {
+        return {
+            
+        }
+    },
+
+    template:
+
+    /*html*/
+
+    ` 
+
+<div class=" pb-3 container-sm">
+
+    <h1 class="title text-center pb-5">Change password</h1>
+
+    <div>
+
+            <form>
+                <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label subtitle-account mb-0">Password</label>
+                <input type="email" class="form-control form-user-login" id="exampleInputEmail1">
+                </div>
+                <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label subtitle-account mb-0">Confirm new password</label>
+                <input type="password" class="form-control form-user-login" id="exampleInputPassword1">
+                </div>
+            </form>
+
+    </div> 
+    
+    
+    <div class="text-center pb-5">
+
+        <button class="btn btn-user mt-5">Send</button>
+
+    </div>
+
+</div>
+        
+    `
+
+})
---
+++ dist/js/components/LoginAdmin.js
@@ -0,0 +1,48 @@
+app.component('login-admin',{
+
+    data() {
+        return {
+            
+        }
+    },
+
+    template:
+
+    /*html*/
+
+    ` 
+
+<div class="pt-4 pb-3">
+
+    <h1 class="mt-5 title-green text-center">Hello Admin</h1>
+
+        <div class="container-sm">
+
+            <div class=" pt-5">
+
+                <form>
+                    <div class="mb-3">
+                    <label for="username" class="form-label subtitle-account mb-0">Username</label>
+                    <input type="email" class="form-control form-user-login" id="username" aria-describedby="emailHelp">
+                    </div>
+                    <div class="">
+                    <label for="password" class="form-label subtitle-account mb-0">Password</label>
+                    <input type="password" class="form-control form-user-login" id="password">
+                    </div>
+                </form>
+
+            </div>  
+
+            <div class="text-center pb-5">
+
+                <button class="btn btn-user mt-5">Login</button>
+
+            </div>
+        
+        </div>    
+
+</div>
+        
+    `
+
+})
